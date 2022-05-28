a2fcb2c967e4106a5087c102acb0da0dde71524b
mugishalinux <67084986+mugishalinux@users.noreply.github.com>
2022-05-28 17:13:34 +0200

    Update UserController.java

--- src/main/java/com/mugisha/hospital/controller/UserController.java
+++ src/main/java/com/mugisha/hospital/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/auth/user")
 @Api(value = "User Endpoints")
