079f18aba73cb763e99ebea75e0437144a8d5414
mugishalinux <67084986+mugishalinux@users.noreply.github.com>
2022-05-28 17:13:07 +0200

    Update DoctorController.java

--- src/main/java/com/mugisha/hospital/controller/DoctorController.java
+++ src/main/java/com/mugisha/hospital/controller/DoctorController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/doctor")
 @Api(value = "Doctor Endpoints")
