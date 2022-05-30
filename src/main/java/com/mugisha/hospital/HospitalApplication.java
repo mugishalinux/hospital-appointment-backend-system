dc5378c6b3db0c06660fa1010b5ddb93ddc95d11
mugishalinux <67084986+mugishalinux@users.noreply.github.com>
2022-05-30 17:48:25 +0200

    Update HospitalApplication.java

--- src/main/java/com/mugisha/hospital/HospitalApplication.java
+++ src/main/java/com/mugisha/hospital/HospitalApplication.java
@@ -1,9 +1,9 @@
 package com.mugisha.hospital;
 
- import com.mugisha.hospital.filters.AuthFilter;
+import com.mugisha.hospital.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -14,16 +14,16 @@ public class HospitalApplication {
 
 	}
 
-	@Bean
-// 	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
-// 		FilterRegistrationBean<AuthFilter> registrationBean=new FilterRegistrationBean<>();
-// 		AuthFilter authFilter=new AuthFilter();
-// 		registrationBean.setFilter(authFilter);
-// 		registrationBean.addUrlPatterns("/api/v1/*");
-// //		registrationBean.addUrlPatterns("/api/v1/department");
-// //		registrationBean.addUrlPatterns("/api/v1/doctor");
-// 		return registrationBean;
-
-// 	}
+//	@Bean
+//	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
+//		FilterRegistrationBean<AuthFilter> registrationBean=new FilterRegistrationBean<>();
+//		AuthFilter authFilter=new AuthFilter();
+//		registrationBean.setFilter(authFilter);
+//		registrationBean.addUrlPatterns("/api/v1/*");
+////		registrationBean.addUrlPatterns("/api/v1/department");
+////		registrationBean.addUrlPatterns("/api/v1/doctor");
+//		return registrationBean;
+//
+//	}
 
 }
